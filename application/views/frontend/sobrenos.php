4636b82b7c2365d09f38329d6a6512c23c61d737
Jhtupi <jhtupi@gmail.com>
2019-04-11 10:46:08 -0300

    Sobrenos view modificada

--- application/views/frontend/sobrenos.php
+++ application/views/frontend/sobrenos.php
@@ -44,32 +44,7 @@
             
                 </div>
                 <br>
-                 <h1 class="page-header">
-                   Nossos autores
-                </h1>
-                <div class="col-md-12 row">
-
-                    <?php
-                    foreach($autores as $autor) {
-                    ?>
-                    <div class="col-md-4 col-xs-6">
-                        <?php 
-                            // Verifica se o usuário tem ou não imagem
-                            if($autor->img == 1) { 
-                                $mostraImg= "assets/frontend/img/usuarios/".md5($autor->id).".jpg"; 
-                            } else {
-                                $mostraImg= "assets/frontend/img/semFoto.png"; 
-                            }
-                        ?>
-                        <img class="img-responsive img-circle" src="<?php echo base_url($mostraImg) ?>" alt="">
-                         <h4 class="text-center">
-                            <a href="<?php echo base_url('autor/'.$autor->id.'/'.limpar($autor->nome)) ?>"><?php echo $autor->nome ?></a>
-                        </h4> 
-                    </div>
-
-                    <?php
-                        }
-                    ?>
+                
                     
                 </div>
 
