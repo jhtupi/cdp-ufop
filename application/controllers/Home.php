c1a7e2a559f0fded3a4d72e018a8a7c160ab31db
Jhtupi <jhtupi@gmail.com>
2019-04-11 10:20:00 -0300

    Banco de dados trocado

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,27 +5,13 @@ class Home extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
-
-		$this->load->model('categorias_model','modelcategorias'); 
-						// Faz a chamada do model dentro do costrutor para chamar ainda que hajam outras funções
-						// 'modelcategorias' é um alias, m apelido para o model
-		$this->categorias = $this->modelcategorias->listar_categorias(); 
-						// variavel categorias que recebe o que foi resgatado pela função
 	}
 	
 	public function index()
 	{
 		$this->load->helper('funcoes');
-		$dados['categorias'] = $this->categorias;
-		// Cria uma variável array chamada dados para receber as categorias
-		// Poderia ter sido chamado direto porém é criada a variável por organização de código
 
-		
-		$this->load->model('publicacoes_model', 'modelpublicacoes');
-		// O modelo de destaques é carregado aqui na função index pois não será requerido em toda pasta como as categorias que ficam no header e aside
-		$dados['postagem'] = $this->modelpublicacoes->destaques_home();
-		// Insere os dados da postagem no array dados
-
+	
 
 		$dados['titulo'] = 'Página Inicial';
 		$dados['subtitulo'] = 'Postagens Recentes';
