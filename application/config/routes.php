2f61e44bbc6b445bf062325b57db66715329dd16
Jhtupi <jhtupi@gmail.com>
2019-05-15 14:29:15 -0300

    Visualizar comunidades quase pronto

---
+++ application/config/routes.php
@@ -0,0 +1,80 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| URI ROUTING
+| -------------------------------------------------------------------------
+| This file lets you re-map URI requests to specific controller functions.
+|
+| Typically there is a one-to-one relationship between a URL string
+| and its corresponding controller class/method. The segments in a
+| URL normally follow this pattern:
+|
+|	example.com/class/method/id/
+|
+| In some instances, however, you may want to remap this relationship
+| so that a different class/function is called than the one
+| corresponding to the URL.
+|
+| Please see the user guide for complete details:
+|
+|	https://codeigniter.com/user_guide/general/routing.html
+|
+| -------------------------------------------------------------------------
+| RESERVED ROUTES
+| -------------------------------------------------------------------------
+|
+| There are three reserved routes:
+|
+|	$route['default_controller'] = 'welcome';
+|
+| This route indicates which controller class should be loaded if the
+| URI contains no data. In the above example, the "welcome" class
+| would be loaded.
+|
+|	$route['404_override'] = 'errors/page_missing';
+|
+| This route will tell the Router which controller/method to use if those
+| provided in the URL cannot be matched to a valid route.
+|
+|	$route['translate_uri_dashes'] = FALSE;
+|
+| This is not exactly a route, but allows you to automatically route
+| controller and method names that contain dashes. '-' isn't a valid
+| class or method name character, so it requires translation.
+| When you set this option to TRUE, it will replace ALL dashes in the
+| controller and method URI segments.
+|
+| Examples:	my-controller/index	-> my_controller/index
+|		my-controller/my-method	-> my_controller/my_method
+*/
+$route['default_controller'] = 'home';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+
+
+// Usuários
+$route['usuario/(:num)/(:any)'] = 'usuarios/usuario/$1/$2'; // Controlador usuarios no método usuario
+$route['meu_perfil/(:num)'] = 'usuarios/meu_perfil/$1'; // Rota para meu perfil
+								
+
+// Login								
+$route['login'] = 'usuarios/pag_login'; // Rota para login
+$route['esqueci_senha'] = 'usuarios/esqueci_senha'; // Rota para esqueci minha senha
+$route['criar_usuario'] = 'usuarios/criar_usuario'; // Rota para criar usuário
+$route['criar_usuario/(:num)'] = 'usuarios/criar_usuario/$1'; // Rota para criar usuário
+
+// Reuniões
+$route['criar_reuniao/(:num)/(:num)/(:num)'] = 'reunioes/criar_reuniao/$1/$2/$3'; // Rota para criar reunião
+$route['criar_reuniao/(:num)/(:num)'] = 'reunioes/criar_reuniao/$1/$2'; // Rota para criar reunião
+$route['reuniao/(:num)'] = 'reunioes/reuniao/$1'; // Rota para visualizar reunião
+
+// Comunidades
+$route['criar_comunidade'] = 'comunidades/criar_comunidade'; // Rota para criar comunidade
+$route['comunidade/(:num)'] = 'comunidades/comunidade/$1'; // Rota para visualizar comunidade
+$route['participar_comunidade/(:num)/(:num)'] = 'comunidades/participar_comunidade/$1/$2'; // Rota para participar da comunidade
+$route['sair_comunidade/(:num)/(:num)'] = 'comunidades/sair_comunidade/$1/$2'; // Rota para sair da comunidade
+
+// Extras
+$route['contato/(:num)'] = 'contato/index/$1'; // Rota para o envio de formulário de contato
