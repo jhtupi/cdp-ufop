ed4163dd7b3470ccbbdf406ff83460e4f9608b38
Jhtupi <jhtupi@gmail.com>
2019-05-16 16:32:28 -0300

    Modal NPS em construção (Visualizar reunião)

--- application/views/frontend/template/aside-reuniao.php
+++ application/views/frontend/template/aside-reuniao.php
@@ -84,3 +84,30 @@
                             } // foreach reuniões
                         } // else $jaAconteceu
                         ?>
+
+                        <!-- Modal para avaliação de NPS -->
+            <div class="modal show bs-example-modal-lg" id="modalEditarCliente" >
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Fechar</span></button>
+                        <h4 class="modal-title">Avaliação NPS da Reunião</h4>
+                      </div>
+                      <div class="modal-body">
+                        
+                        <form role="form" method="post" action="<?= base_url('index.php/clientes/salvar')?>" id="formulario_clientes">
+                          <div class="form-group">
+                            <label for="nps-reuniao">De 1 a 10, o quanto você indicaria uma reunião desta comunidade para um amigo ou colega?</label>
+                            (1- Jamais indicaria | 10- Indicaria com certeza)
+                            <input type="number" class="form-control" min="1" max="10" id="nps-reuniao" name="nps-reuniao">
+                          </div>
+                        </form>     
+                            
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-primary" onclick="$('#formulario_clientes').submit()">Avaliar reunião</button>
+                      </div>
+                    </div><!-- /.modal-content -->
+                  </div><!-- /.modal-dialog -->
+                </div><!-- /.modal -->  
